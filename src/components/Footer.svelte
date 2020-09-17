9d53b3a0ed28ba5cdeca42e7bda9b043af191467
Igor16 <z.igor16@gmail.com>
2020-09-17 14:55:08 +0200

    Add Footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,9 @@
+<script>
+    let this_year = new Date().getFullYear();
+</script>
+
+<div class="w-full d-flex justify-center p-3 bg-dark">
+    <h6 class="text-sm text-light text-center">
+        &copy; Copyright {this_year}, All Rights Reserved. Made by Igor Zanella
+    </h6>
+</div>
