35e6911db6ca6616e6ed1fc01904144dd815eab9
Igor16 <z.igor16@gmail.com>
2020-09-17 16:03:43 +0200

    Add links and fix html code

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,7 @@
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Home - igorzanella.dev</title>
 </svelte:head>
 
 <div class="py-2">
@@ -10,18 +10,18 @@
 		<div class="h-64 sm:h-12">
 			<img class="rounded-full object-cover object-top h-full w-auto" src="img/igor.jpeg" alt="Igor Zanella"/>
 		</div>
-		<div class="bg-white w-3/4 rounded-lg shadow-lg p-4 -mt-12">
+		<div class="bg-white w-3/4 rounded-lg shadow-lg p-4 -mt-16">
 			<p class="text-center text-2xl font-bold text-dark">Hi, I'm Igor Zanella!</p>
 			<p class="text-center font-semibold text-primary">Full Stack Developer<br/>IT Consultant</p>
 		</div>
 	</div>
-	<div class="my-2 bg-primary flex justify-center align-items">
-		<div class="w-5/6 h-5/6 m-4 bg-light shadow-2xl rounded-lg">
+	<div class="my-2  flex justify-center align-items">
+		<a href="projects" class="w-5/6 h-5/6 m-5 bg-light shadow-2xl rounded-lg">
 			<img class="rounded-t-lg" src="img/code.jpg" alt="Developer Projects"/>
 			<div class="p-3">
 				<h3 class="font-semibold text-primary mb-2 text-xl">Projects</h3>
 				<p>Discover my projects in the dedicated section.</p>
 			</div>
-		</div>
+		</a>
 	</div>
 </div>
