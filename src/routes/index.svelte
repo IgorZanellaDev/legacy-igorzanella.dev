df2de5a1bef8c426bebfd53019739efd7ee4c1dc
Igor16 <z.igor16@gmail.com>
2020-09-17 14:56:31 +0200

    Start index page with test photos

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,27 @@
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
 </style>
 
 <svelte:head>
 	<title>Sapper project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<div class="py-2">
+	<div class="flex flex-col justify-content-center items-center">
+		<div class="h-64 sm:h-12">
+			<img class="rounded-full object-cover object-top h-full w-auto" src="img/igor.jpeg" alt="Igor Zanella"/>
+		</div>
+		<div class="bg-white w-3/4 rounded-lg shadow-lg p-4 -mt-12">
+			<p class="text-center text-2xl font-bold text-dark">Hi, I'm Igor Zanella!</p>
+			<p class="text-center font-semibold text-primary">Full Stack Developer<br/>IT Consultant</p>
+		</div>
+	</div>
+	<div class="my-2 bg-primary flex justify-center align-items">
+		<div class="w-5/6 h-5/6 m-4 bg-light shadow-2xl rounded-lg">
+			<img class="rounded-t-lg" src="img/code.jpg" alt="Developer Projects"/>
+			<div class="p-3">
+				<h3 class="font-semibold text-primary mb-2 text-xl">Projects</h3>
+				<p>Discover my projects in the dedicated section.</p>
+			</div>
+		</div>
+	</div>
+</div>
