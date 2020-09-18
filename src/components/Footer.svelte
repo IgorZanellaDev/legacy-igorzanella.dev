5858af3a85b4c8428599ff623885d0b355c3a615
Igor16 <z.igor16@gmail.com>
2020-09-18 03:05:37 +0200

    Add html br in footer for mobile

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,6 +4,6 @@
 
 <div class="w-full d-flex justify-center p-3 z-10">
     <h6 class="text-sm text-light text-center">
-        &copy; Copyright {this_year}, All Rights Reserved. Made by Igor Zanella
+        &copy; Copyright {this_year}, All Rights Reserved.<br class="sm:hidden"/> Made by Igor Zanella
     </h6>
 </div>
