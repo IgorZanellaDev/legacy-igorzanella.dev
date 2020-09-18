eb3fab10819fd8c13e1debb3dfa48e1e4339e5e2
Igor16 <z.igor16@gmail.com>
2020-09-18 03:05:52 +0200

    Remove fixed height of cards

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -16,16 +16,16 @@
 		</div>
 	</div>
 	<div class="my-2 flex justify-center items-center flex-col">
-		<a href="projects" class="w-5/6 h-72 m-5 bg-light shadow-2xl rounded-lg flex flex-col md:flex-row">
+		<a href="projects" class="w-5/6 m-5 bg-light shadow-2xl rounded-lg flex flex-col md:flex-row">
 			<img class="rounded-t-lg object-cover w-full h-48 object-center md:w-1/2 md:rounded-none md:rounded-l-lg md:h-full" src="img/bod_home.jpeg" alt="Developer Projects"/>
 			<div class="p-3 md:flex-grow">
 				<h3 class="font-semibold text-primary mb-2 text-xl">Projects</h3>
 				<p>Discover my projects in the dedicated section.</p>
 			</div>
 		</a>
-		<a href="articles" class="w-5/6 h-72 m-5 bg-light shadow-2xl rounded-lg flex flex-col md:flex-row-reverse">
+		<a href="articles" class="w-5/6 m-5 bg-light shadow-2xl rounded-lg flex flex-col md:flex-row-reverse">
 			<img class="rounded-t-lg object-cover w-full h-48 object-center md:w-1/2 md:rounded-none md:rounded-r-lg md:h-full" src="img/article.jpg" alt="Articles"/>
-			<div class="p-3 md:flex-grow ">
+			<div class="p-3 md:flex-grow">
 				<h3 class="font-semibold text-primary mb-2 text-xl">Articles</h3>
 				<p>Read the articles I written on the web.</p>
 			</div>
