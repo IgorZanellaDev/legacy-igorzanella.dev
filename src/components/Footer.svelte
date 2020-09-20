350c90c31d160878af4ac6327519f2029e6627c7
Igor16 <z.igor16@gmail.com>
2020-09-20 15:31:35 +0200

    Modify Footer component

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,6 +4,6 @@
 
 <div class="w-full d-flex justify-center p-3 z-10">
     <h6 class="text-sm text-light text-center">
-        &copy; Copyright {this_year}, All Rights Reserved.<br class="sm:hidden"/> Made by Igor Zanella
+        &copy; Copyright {this_year}, All Rights Reserved.<br class="sm:hidden"/> Made by Igor Zanella <br class="sm:hidden"/><span class="hidden sm:inline">|</span> VAT: IT05104380265
     </h6>
 </div>
