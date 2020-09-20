aa58d35781559b048f04ce1dcf8142ff0cc67bef
Igor16 <z.igor16@gmail.com>
2020-09-20 15:33:17 +0200

    Start about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -2,6 +2,20 @@
 	<title>About - igorzanella.dev</title>
 </svelte:head>
 
-<h1>About this site</h1>
-
-<p>This is the 'about' page. There's not much here.</p>
+<!--<div class="flex flex-col justify-content-center items-center sm:flex-row sm:px-2">
+	<div class="h-56 sm:flex-grow sm:flex sm:items-center sm:justify-center {animations.home_mobile ? "animate__animated animate__fadeInLeft" : ""}">
+		<img class="rounded-full object-cover object-top h-full w-auto" src="img/igor.jpeg" alt="Igor Zanella"/>
+	</div>
+	<div class="bg-white w-5/6 rounded-lg shadow-lg p-4 -mt-16 border-none {animations.home_mobile ? "animate__animated animate__fadeInRight" : ""} sm:hidden">
+		<Typewriter interval={tw_interval} delay={100} cascade active={animations.home_mobile} on:done={handleAnimationMobileDone}>
+		<p class="text-center text-2xl font-bold text-dark"><span>Hi, I'm </span> <span class="text-primary">Igor Zanella</span><span>!</span></p>
+		<p class="text-center font-semibold text-secondary">Full Stack Developer</p>
+		<p class="text-center font-semibold text-secondary">IT Consultant</p>
+		</Typewriter>
+	</div>
+	<div class="hidden sm:flex sm:items-center sm:flex-grow">
+		<Typewriter interval={tw_interval} cascade active={animations.home_desktop} on:done={handleAnimationDesktopDone}>
+			<h2>Hi, I'm Igor Zanella!</h2>
+		</Typewriter>
+	</div>
+</div>-->
