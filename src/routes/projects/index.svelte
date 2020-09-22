8b7277001ed90f36ef486b4abe58c7916bcd6ea9
Igor16 <z.igor16@gmail.com>
2020-09-22 16:05:09 +0200

    Change projects load behaviour and handle button click

--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -1,8 +1,7 @@
 <script context="module">
+	import {getProjects} from "../../assets/projects.js";
 	export function preload({ params, query }) {
-		return this.fetch(`projects.json`).then(r => r.json()).then(projects => {
-			return { projects };
-		});
+		return { projects: getProjects() }
 	}
 </script>
 
@@ -11,7 +10,22 @@
 	import { fade } from 'svelte/transition';
 	import { goto } from '@sapper/app';
 	export let projects;
-	let animateOut = false;
+	let animate = false;
+
+	function handleClick(i){
+		projects.forEach(project => {
+			project.clicked = false;
+			project.animateOut = false;
+		})
+		projects[i].clicked = true;
+		projects.forEach((project, z) => {
+			if(z !== i){
+				project.animateOut = true;
+			}
+		});
+		animate = true;
+		setTimeout(() => goto('/projects/'+projects[i].slug), 400);
+	}
 </script>
 
 <style>
@@ -25,14 +39,14 @@
 	<h1 class="text-light text-3xl font-title">My <span class="text-primary">projects</span></h1>
 	<div class="w-full flex flex-col items-center justify-center perspective-500">
 		{#each projects as project, i}
-			<button on:click={() => {project.clicked = true; animateOut = true; setTimeout(()=> goto('/projects/'+project.slug), 400)}} class="bg-light w-5/6 m-2 rounded-lg flex flex-col focus:outline-none sm:m-4 {i % 2 ? "sm:flex-row-reverse" : "sm:flex-row"} {animateOut ? project.clicked ? "fade-out-fwd z-50" : "fade-out" : "hover:vibrate-1"} sm:w-full">
+			<button on:click={() => handleClick(i)} class="bg-light w-5/6 m-2 rounded-lg flex flex-col focus:outline-none sm:m-4 {i % 2 ? "sm:flex-row-reverse" : "sm:flex-row"} {project.clicked && animate ? "fade-out-fwd z-50" : project.animateOut && animate ? "fade-out" : "hover:vibrate-1"} sm:w-full">
 				<a class="hidden" href="/projects/{project.slug}">{project.slug}</a>
 				<img class="rounded-t-lg object-cover w-full h-48 object-center sm:flex-grow sm:w-1/2 sm:h-56 {i % 2 ? "sm:rounded-none sm:rounded-r-lg" : "sm:rounded-none sm:rounded-l-lg"}" src="img/projects/{project.image}" alt={project.title}/>
 				<div class="p-3 text-left">
 					<h2 class="font-semibold text-primary text-xl">{project.title}</h2>
 					<div class="flex flex-row flex-grow-0 flex-wrap -px-1">
 						{#each project.tags as tag}
-							<Tag tag={tag} classes="mr-2 my-2"/>
+							<Tag tag={tag} clickable={false} classes="mr-2 my-2"/>
 						{/each}
 					</div>
 					<p class="mt-2">{project.description}</p>
