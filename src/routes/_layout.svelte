cd74e500df602c98fcd651f7153d2f8aead5a702
Igor16 <z.igor16@gmail.com>
2020-09-23 18:09:26 +0200

    Add Tawk Chat

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,10 +1,23 @@
 <script>
+	import { onMount } from 'svelte';
 	import Nav from '../components/Nav.svelte';
 	import GlobalStyle from '../components/GlobalStyle.svelte';
 	import Footer from '../components/Footer.svelte';
 	import Background from '../components/Background.svelte';
 
 	export let segment;
+
+	onMount(() => {
+		var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+		(function(){
+			var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+			s1.async=true;
+			s1.src='https://embed.tawk.to/5f5969184704467e89eda1f9/default';
+			s1.charset='UTF-8';
+			s1.setAttribute('crossorigin','*');
+			s0.parentNode.insertBefore(s1,s0);
+		})();
+	})
 </script>
 
 <GlobalStyle/>
