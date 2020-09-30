26f2d3d052494f2c36ec82cc0de0c624a248bed6
Igor16 <z.igor16@gmail.com>
2020-09-30 22:27:41 +0200

    Modify policies scripts position

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,13 @@
 <script>
     import Fa from 'svelte-fa';
     import { faTwitterSquare, faDev, faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-    
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        (function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);
+        (function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);
+    });
+
     let this_year = new Date().getFullYear();
 </script>
 
@@ -13,8 +19,8 @@
         <a href="https://www.linkedin.com/in/igor-zanella/" target="_blank" rel="noopener noreferrer" class="text-light mx-2 flex items-center transition-all duration-300 hover:text-linkedin"><Fa icon={faLinkedin} class="text-3xl sm:mr-1"/><span class="hidden sm:flex">Linkedin</span></a>
     </div>
     <div class="flex mt-4 mx-auto">
-        <a href="https://www.iubenda.com/privacy-policy/36569385" class="iubenda-black no-brand iubenda-embed" title="Privacy Policy ">Privacy Policy</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
-        <a href="https://www.iubenda.com/privacy-policy/36569385/cookie-policy" class="iubenda-black no-brand iubenda-embed" title="Cookie Policy ">Cookie Policy</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
+        <a href="https://www.iubenda.com/privacy-policy/36569385" class="iubenda-black no-brand iubenda-embed" title="Privacy Policy ">Privacy Policy</a>
+        <a href="https://www.iubenda.com/privacy-policy/36569385/cookie-policy" class="iubenda-black no-brand iubenda-embed" title="Cookie Policy ">Cookie Policy</a>
     </div>
     <h6 class="text-sm text-light text-center mt-2">
         &copy; Copyright {this_year}, All Rights Reserved.<br class="sm:hidden"/> Made by Igor Zanella <br class="sm:hidden"/><span class="hidden sm:inline">|</span> VAT: IT05104380265
