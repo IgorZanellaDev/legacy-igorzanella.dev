b9268a2b9d9420e654d5922615a89d3192b851ea
Igor16 <z.igor16@gmail.com>
2020-10-01 01:26:57 +0200

    Fix Iubenda policies link in Footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -12,11 +12,7 @@
         <a href="https://dev.to/igorzanelladev" target="_blank" rel="noopener noreferrer" class="text-light mx-2 flex items-center transition-all duration-300 hover:text-secondary"><Fa icon={faDev} class="text-3xl sm:mr-1"/><span class="hidden sm:flex">Dev.to</span></a>
         <a href="https://www.linkedin.com/in/igor-zanella/" target="_blank" rel="noopener noreferrer" class="text-light mx-2 flex items-center transition-all duration-300 hover:text-linkedin"><Fa icon={faLinkedin} class="text-3xl sm:mr-1"/><span class="hidden sm:flex">Linkedin</span></a>
     </div>
-    <div class="flex mt-4 mx-auto">
-        <a href="https://www.iubenda.com/privacy-policy/36569385" class="iubenda-black no-brand iubenda-embed" title="Privacy Policy ">Privacy Policy</a>
-        <a href="https://www.iubenda.com/privacy-policy/36569385/cookie-policy" class="iubenda-black no-brand iubenda-embed" title="Cookie Policy ">Cookie Policy</a>
-    </div>
     <h6 class="text-sm text-light text-center mt-2">
-        &copy; Copyright {this_year}, All Rights Reserved.<br class="sm:hidden"/> Made by Igor Zanella <br class="sm:hidden"/><span class="hidden sm:inline">|</span> VAT: IT05104380265
+        &copy; Copyright {this_year}, All Rights Reserved.<br class="sm:hidden"/> Made by Igor Zanella <br class="sm:hidden"/><span class="hidden sm:inline">|</span> VAT: IT05104380265 <br class="sm:hidden"/><span class="hidden sm:inline">|</span> <a href="https://www.iubenda.com/privacy-policy/36569385" class="underline" title="Privacy Policy" target="_blank" rel="noopener noreferrer">Privacy Policy</a> | <a href="https://www.iubenda.com/privacy-policy/36569385/cookie-policy" class="underline" title="Cookie Policy" target="_blank" rel="noopener noreferrer">Cookie Policy</a>
     </h6>
 </div>
