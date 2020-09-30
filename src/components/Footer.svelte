30b7cb0f49fade555f8dd4cff7f1fe01b1c35f45
Igor16 <z.igor16@gmail.com>
2020-10-01 00:17:34 +0200

    Test for iubenda buttons script

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,15 +1,13 @@
 <script>
     import Fa from 'svelte-fa';
     import { faTwitterSquare, faDev, faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-    import { onMount } from "svelte";
-
-    onMount(() => {
-        (function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);
-        (function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);
-    });
 
     let this_year = new Date().getFullYear();
 </script>
+
+<svelte:head>
+	<script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>
+</svelte:head>
 
 <div class="w-full flex flex-col justify-center p-3 z-10">
     <div class="mx-auto flex">
