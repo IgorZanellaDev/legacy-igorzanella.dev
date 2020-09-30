1b9b290677bbe466b50e708cbc9a5b233065190c
Igor16 <z.igor16@gmail.com>
2020-09-30 17:54:10 +0200

    Modify env file for production

--- src/components/env.js
+++ src/components/env.js
@@ -1,3 +1,3 @@
-export const URL_SITE = "https://igorzanella.netlify.app/";
+export const URL_SITE = "https://igorzanella.dev/";
 export const DEFAULT_META_IMG = "img/meta-img.jpg";
 export const SITE_NAME = "igorzanella.dev";
