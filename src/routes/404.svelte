9bd14dc1a666751918f46e043b1b57d89d7e3707
Igor16 <z.igor16@gmail.com>
2020-10-01 02:02:27 +0200

    Fix for Netlify 404 error

---
+++ src/routes/404.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<title>404 error - igorzanella.dev</title>
+</svelte:head>
+
+<div class="absolute w-full h-full flex flex-col items-center justify-center flicker-1">
+	<h1 class="text-light text-4xl font-title font-bold sm:text-6xl lg:text-7xl">ERROR {status}</h1>
+	<h2 class="text-light text-3xl font-bold sm:text-5xl lg:text-6xl">{error.message}</h2>
+	<a class="text-light mt-2 bg-primary hover:bg-primaryDark transition-all duration-300 cursor-pointer px-4 py-2 rounded-lg text-2xl sm:text-3xl lg:text-4xl" href=".">Go to home</a>
+</div>
