48e1d2f5b49a726d6e969530f083aa19dd65e028
Igor16 <z.igor16@gmail.com>
2020-10-01 02:04:03 +0200

    Fix html errors

--- src/routes/404.svelte
+++ src/routes/404.svelte
@@ -3,7 +3,7 @@
 </svelte:head>
 
 <div class="absolute w-full h-full flex flex-col items-center justify-center flicker-1">
-	<h1 class="text-light text-4xl font-title font-bold sm:text-6xl lg:text-7xl">ERROR {status}</h1>
-	<h2 class="text-light text-3xl font-bold sm:text-5xl lg:text-6xl">{error.message}</h2>
+	<h1 class="text-light text-4xl font-title font-bold sm:text-6xl lg:text-7xl">ERROR 404</h1>
+	<h2 class="text-light text-3xl font-bold sm:text-5xl lg:text-6xl">NOT FOUND</h2>
 	<a class="text-light mt-2 bg-primary hover:bg-primaryDark transition-all duration-300 cursor-pointer px-4 py-2 rounded-lg text-2xl sm:text-3xl lg:text-4xl" href=".">Go to home</a>
 </div>
