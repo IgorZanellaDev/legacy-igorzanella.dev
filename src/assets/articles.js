8962d2f899e7349d99a6778b57e4dd66e11fe014
Igor16 <z.igor16@gmail.com>
2020-10-02 02:39:54 +0200

    Add new article

--- src/assets/articles.js
+++ src/assets/articles.js
@@ -6,6 +6,14 @@ const articles = [
         description: "Simple tutorial on CSS buttons, I wrote it to try the platform.",
         tags: ["html", "css"],
         image: "simple-3d-css-btns.jpg"
+    },
+    {
+        title: "Light Switch canvas background",
+        platform: "DEV.to",
+        link: "https://dev.to/igorzanelladev/light-switch-canvas-responsive-background-l4a",
+        description: "Tutorial on a canvas I did for test.",
+        tags: ["html", "css", "javascript", "canvas"],
+        image: "lightswitch.gif"
     }
 ];
 
